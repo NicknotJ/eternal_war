e744165c2bc3f982bdd2e05e2646c1eabfd2fceb
nicknotj <nicknotjustin@gmail.com>
2019-08-21 23:06:39 -0500

    future comments and added a handWinner parameter to warGame.

--- war.js
+++ war.js
@@ -1,7 +1,10 @@
 class warGame {
   constructor(numPlayers, pile = [], players = []){
     this.numPlayers = numPlayers;
+    //this will signal game end if defined
     this.winner = undefined;
+    //this simply signals who should get all the cards
+    this.handWinner = undefined;
     this.pile = pile;
     this.players = players;
   }
